f62fc1962f3fb072736b7411e877a5c55b4e4f34
Travis CI <najwer23@gmail.com>
2019-10-30 01:11:14 +0100

    prototype calendar in JS

--- JS/calendar.js
+++ JS/calendar.js
@@ -1,1 +1,18 @@
-console.log("jupi")
+window.onload = function () {
+
+    let field = [".yearTens", ".yearThousands", ".yearUnits", ".monthRow", ".fourRow", ".dayUnits"]
+    for (let i=0; i<field.length; i++) {
+        document.querySelectorAll(''+field[i]+'').forEach(e => {
+            e.addEventListener('click', function () {
+                check(e)
+            })
+        });
+    }
+    
+
+
+    function check(e){
+        let val = e.textContent;
+        console.log(e.className+" "+val)
+    }
+}
